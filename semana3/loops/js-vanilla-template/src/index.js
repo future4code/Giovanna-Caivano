8467e1e28c40d24840e3667e738b84c4b88be4ff
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-07-30 14:36:29 -0300

    Exercícios e desafios com loops

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,9 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+//1. O código pede o input de número ao usuário, transforma o input em número e verifica se o número inserido pelo usuário é par ou ímpar. Caso o número seja par, ele apresenta a msg "Passou no teste", caso seja ímpar, apresenta a msg "Não passou no teste".
+
+//2. a. O código serve para imprimir o preço da fruta informada pelo usuário.
+//b. Para a fruta Maçã a msg impressa será: O preço da fruta Maçã é R$ 2.25
+//c. Caso não houvesse o break do item c, o switch continuaria rodando e a msg impressa seria:
+// O preço da fruta Pêra é R$ 5
+
+//3. a. A primeira linha está pedindo um input de número ao usuário e transformando-o de string para número.
