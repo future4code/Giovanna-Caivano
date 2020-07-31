237999a3771b2964235fa728835a17eeee1e6b0f
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-07-31 11:33:43 -0300

    Correção

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,26 +1,6 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+console.log("Bem vindo ao jogo de Blackjack!")
+let play = confirm("Quer iniciar uma nova rodada?")
 
- console.log("Bem vindo ao jogo de Blackjack!")
- let play = confirm("Quer iniciar uma nova rodada?")
-
-if (play) {
-   console.log("Vamos jogar!!!")
-} else {
-   console.log("O jogo acabou!")
-}
-
-//RODADA
 //variáveis do usuário
 let playerHand = []
 let playerPoints = []
@@ -33,50 +13,59 @@ let pcPoints = []
 let printPcHand = ""
 let printPcPoints = 0
 
-//sorteia 2 cartas para o jogador
-for (let i=0; i <=2; i++) {
-   let carta = comprarCarta()
-   playerHand.push(carta.texto)
-   playerPoints.push(carta.valor)
-   i++
-}
+if (play) {
+   console.log("Vamos jogar!!!")
 
-for (let carta of playerHand) {
-   printPlayerHand += (carta + " ")
-}
+      //RODADA
+   //sorteia 2 cartas para o jogador
+   for (let i=0; i <=2; i++) {
+      let carta = comprarCarta()
+      playerHand.push(carta.texto)
+      playerPoints.push(carta.valor)
+      i++
+   }
 
-
-//sorteia 2 cartas para o computador
-for (let i=0; i <=2; i++) {
-   let carta = comprarCarta()
-   pcHand.push(carta.texto)
-   pcPoints.push(carta.valor)
-   i++
-}
-
-for (let carta of pcHand) {
-   printPcHand += (carta + " ")
-}
+   for (let carta of playerHand) {
+      printPlayerHand += (carta + " ")
+   }
 
 
-//define a pontuação de cada jogador
-for (let valor of playerPoints) {
-   printPlayerPoints += valor
-}
+   //sorteia 2 cartas para o computador
+   for (let i=0; i <=2; i++) {
+      let carta = comprarCarta()
+      pcHand.push(carta.texto)
+      pcPoints.push(carta.valor)
+      i++
+   }
 
-for (let valor of pcPoints) {
-   printPcPoints += valor
-}
+   for (let carta of pcHand) {
+      printPcHand += (carta + " ")
+   }
 
-//mostra cartas e pontuação
-console.log(`Usuário - cartas: ${printPlayerHand} - pontuação ${printPlayerPoints}`)
-console.log(`Computador - cartas: ${printPcHand} - pontuação ${printPcPoints}`)
 
-//informa o vencedor ou empate
-if (printPlayerPoints === printPcPoints) {
-   console.log("Empate!")
-} else if (printPlayerPoints > printPcPoints) {
-   console.log("O usuário ganhou!")
+   //define a pontuação de cada jogador
+   for (let valor of playerPoints) {
+      printPlayerPoints += valor
+   }
+
+   for (let valor of pcPoints) {
+      printPcPoints += valor
+   }
+
+   //mostra cartas e pontuação
+   console.log(`Usuário - cartas: ${printPlayerHand} - pontuação ${printPlayerPoints}`)
+   console.log(`Computador - cartas: ${printPcHand} - pontuação ${printPcPoints}`)
+
+   //informa o vencedor ou empate
+   if (printPlayerPoints === printPcPoints) {
+      console.log("Empate!")
+   } else if (printPlayerPoints > printPcPoints) {
+      console.log("O usuário ganhou!")
+   } else {
+      console.log("O computador ganhou!")
+   }
 } else {
-   console.log("O computador ganhou!")
+   console.log("O jogo acabou!")
+   
 }
+
