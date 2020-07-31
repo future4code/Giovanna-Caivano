b172e8f1cc320fcc4001578d87372ee28085d706
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-07-31 19:43:57 -0300

    Desafio até o item 10

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,105 +11,119 @@
  * 
  */
 
-// console.log("Bem vindo ao jogo de Blackjack!")
-// let play = confirm("Quer iniciar uma nova rodada?")
+console.log("Bem vindo ao jogo de Blackjack!")
+let play = confirm("Quer iniciar uma nova rodada?")
 
-// //variáveis do usuário
-// let playerHand = []
-// let playerPoints = []
-// let printPlayerHand = ""
-// let printPlayerPoints = 0
+//variáveis do usuário
+let playerHand = []
+let playerPoints = []
+let printPlayerHand = ""
+let printPlayerPoints = 0
 
-// //variáveis do computador
-// let pcHand = []
-// let pcPoints = []
-// let printPcHand = ""
-// let printPcPoints = 0
-
-// //8 - verifica se ambas as cartas são ases
-// let verifyPlayerHand = (playerPoints[0] === 11) && (playerPoints[1] === 11)
-// let verifyPcHand = (pcPoints[0] === 11) && (pcPoints[1] === 11)
-
-// if (play) {
-//    console.log("Vamos jogar!!!")
+//variáveis do computador
+let pcHand = []
+let pcPoints = []
+let printPcHand = ""
+let printPcPoints = 0
 
 
-//    //RODADA
-//    //sorteia 2 cartas para o jogador
-//    for (let i=0; i <=2; i++) {
-//       let carta = comprarCarta()
-//       playerHand.push(carta.texto)
-//       playerPoints.push(carta.valor)
-//       if (verifyPlayerHand) {
-//          i = i
-//       } else {
-//          i++
-//       }
-//    }
-
-//    for (let carta of playerHand) {
-//       printPlayerHand += (carta + " ")
-//    }
+if (play) {
+   console.log("Vamos jogar!!!")
 
 
-//    //sorteia 2 cartas para o computador
-//    for (let i=0; i <=2; i++) {
-//       let carta = comprarCarta()
-//       pcHand.push(carta.texto)
-//       pcPoints.push(carta.valor)
-//       if (verifyPcHand) {
-//          i = i
-//       } else {
-//          i++
-//       }
-//    }
+   //RODADA
+   //sorteia 2 cartas para o jogador e verifica se ambas as cartas são ases
+   for (let i=0; i < 2;) {
+      let carta = comprarCarta()
+      playerHand.push(carta.texto)
+      playerPoints.push(carta.valor)
+      if ((playerPoints[0] === 11) && (playerPoints[1] === 11)) {
+         i = i
+      } else {
+         i++
+      }
+   }
 
-//    for (let carta of pcHand) {
-//       printPcHand += (carta + " ")
-//    }
-
-//    //define a pontuação de cada jogador
-//    for (let valor of playerPoints) {
-//       printPlayerPoints += valor
-//    }
-
-//    for (let valor of pcPoints) {
-//       printPcPoints += valor
-//    }
-
-//    //9 - Verificar se algum dos jogadores chegou a 21 e pergunta se o jogador quer comprar outra carta, mostra suas duas cartas e esconde a segunda carta do computador
-//    if (printPlayerPoints === 21 || printPcPoints === 21) {
-//       let winner = "computador"
-//       if (printPlayerPoints === 21) {
-//          winner = "usuário"
-//       }
-//       alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
-//       "\n"+
-//       `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}`+
-//       `O ${winner} ganhou!`
-//       )
-//    } else {
-//       let buyCard = confirm(`Suas cartas são ${printPlayerHand}. A carta revelada do computador é ${printPcHand[0]+printPcHand[1]}`+ "\n"+ "Deseja comprar mais uma carta?")
-
-//       if (buyCard) {
-//          let carta = comprarCarta()
-//          playerHand.push(carta.texto)
-//          playerPoints.push(carta.valor)
-//          printPlayerHand += (carta.texto + " ")
-//          printPlayerPoints += carta.valor
-//       } else {
-//          let winner = "computador"
-//          if (printPlayerPoints < printPcPoints) {
-//             winner = "usuário"
-//          }
-//          alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
-//       "\n" + `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}.`+"\n"+ `O ${winner} ganhou!`)
-//       }
+   for (let carta of playerHand) {
+      printPlayerHand += (carta + " ")
+   }
 
 
-//    }
+   //sorteia 2 cartas para o computador
+   for (let i=0; i < 2;) {
+      let carta = comprarCarta()
+      pcHand.push(carta.texto)
+      pcPoints.push(carta.valor)
+      if ((pcPoints[0] === 11) && (pcPoints[1] === 11)) {
+         i = i
+      } else {
+         i++
+      }
+   }
 
-// } else {
-//    console.log("O jogo acabou!")
-// }
+   for (let carta of pcHand) {
+      printPcHand += (carta + " ")
+   }
+
+   //define a pontuação de cada jogador
+   for (let valor of playerPoints) {
+      printPlayerPoints += valor
+   }
+
+   for (let valor of pcPoints) {
+      printPcPoints += valor
+   }
+
+   
+   //9 - Verificar se algum dos jogadores chegou a 21 e pergunta se o jogador quer comprar outra carta, mostra suas duas cartas e esconde a segunda carta do computador
+   if (printPlayerPoints === 21 || printPcPoints === 21) {
+      let winner = "computador"
+      if (printPlayerPoints === 21) {
+         winner = "usuário"
+      }
+      alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
+      "\n"+
+      `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}`+
+      `O ${winner} ganhou!`
+      )
+   } else {
+      let gameOn = true
+
+      while (gameOn){
+         let buyCard = confirm(`Suas cartas são ${printPlayerHand}. A carta revelada do computador é ${printPcHand[0]+printPcHand[1]}`+ "\n"+ "Deseja comprar mais uma carta?")
+         if (buyCard) {
+            let carta = comprarCarta()
+            playerHand.push(carta.texto)
+            playerPoints.push(carta.valor)
+            printPlayerHand += (carta.texto + " ")
+            printPlayerPoints += carta.valor
+
+            if (printPlayerPoints === 21) {
+               gameOn = false
+               alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
+         "\n" + `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}.`+"\n"+ `O usuário ganhou!`)
+            } else if (printPlayerPoints > 21) {
+               gameOn = false
+               alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
+         "\n" + `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}.`+"\n"+ `O computador ganhou!`)
+            } else {
+               gameOn = true
+            }
+
+         } else {
+            gameOn = false
+            let winner = "computador"
+            if (printPlayerPoints > printPcPoints) {
+               winner = "usuário"
+            }
+            alert(`Suas cartas são ${printPlayerHand}. Sua pontuação é ${printPlayerPoints}`+
+         "\n" + `As cartas do computador são ${printPcHand}. A pontuação do computador é ${printPcPoints}.`+"\n"+ `O ${winner} ganhou!`)
+         }
+      }
+
+   }
+
+} else {
+   console.log("O jogo acabou!")
+}
  
