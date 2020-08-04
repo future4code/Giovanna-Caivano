a548af9e8c9200ec5aed1c94beaf384a325d5632
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-08-04 16:03:49 -0300

    Exercícios e desafios

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -2,16 +2,23 @@
 function createTodo() {
     const myTodo = document.getElementById("tarefa").value
     const whenTodo = document.getElementById("dias-semana").value
+    const timeTodo = document.getElementById("hour-todo").value
     
     if (myTodo !== "") {
-        document.getElementById(whenTodo).innerHTML += `<p onclick='deleteTodo()'>${myTodo}</p>`
+        document.getElementById(whenTodo).innerHTML += `<li onclick='deleteTodo(this)'>${timeTodo} - ${myTodo}</li>`
         document.getElementById("tarefa").value = ""
     } else {
         alert("Por favor, digite uma tarefa!")
     }
 }
 
-function deleteTodo() {
-    const todo = document.querySelectorAll("p").innerHTML
-    console.log(todo)
+function deleteTodo(element) {
+    element.className = "done"
+}
+
+function cleanPlanner() {
+    const daysOfTheWeek = document.getElementsByTagName("ul")
+    for (i=0; i<daysOfTheWeek.length; i++) {
+        daysOfTheWeek[i].innerHTML = ""
+    }
 }
