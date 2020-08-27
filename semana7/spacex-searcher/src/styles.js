fedecf8dcc534293965f4f9065767abfc683cc4c
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-08-27 16:44:44 -0300

    SpaceX Launches Search Engine.

---
+++ semana7/spacex-searcher/src/styles.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+    text-align: center;
+    margin: 20px 20px;
+`
+
+export const Container = styled.div`
+    text-align: center;
+`
+export const LogoImg = styled.img`
+    width: 50px;
+`
+
+export const Title = styled.h1`
+    font-family: 'Russo One', sans-serif;
+`
+export const SubTitle = styled.h1`
+    &:hover {
+        text-shadow: 2px 2px yellowgreen;
+    }
+`
+
