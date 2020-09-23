858cecaae0959cb32aaa07e2517c4460d94fa0a8
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-09-23 13:50:37 -0300

    Conclusão dos testes e implementação de novas funcionalidades

--- semana11/testes-react/src/App.js
+++ semana11/testes-react/src/App.js
@@ -5,6 +5,7 @@ import { Post } from "./components/Post";
 const App = () => {
   const [postsList, setPostsList] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [errorMsgVisibility, setErrorMsgVisibility] = useState(false);
 
   const onChangeInput = event => {
     setInputValue(event.target.value);
@@ -12,16 +13,21 @@ const App = () => {
 
   const addPost = () => {
     // Adiciona um post à lista
-    const newPost = {
-      id: Date.now(),
-      text: inputValue,
-      liked: false
-    };
-
-    const newPostsList = [newPost, ...postsList];
-
-    setPostsList(newPostsList);
-    setInputValue("")
+    if(inputValue){
+      const newPost = {
+        id: Date.now(),
+        text: inputValue,
+        liked: false
+      };
+  
+      const newPostsList = [newPost, ...postsList];
+  
+      setPostsList(newPostsList);
+      setInputValue("")
+      setErrorMsgVisibility(false)
+    } else {
+      setErrorMsgVisibility(true)
+    }
   };
 
   const deletePost = postId => {
@@ -62,6 +68,8 @@ const App = () => {
         <button onClick={addPost}>Adicionar</button>
       </div>
       <br />
+      {errorMsgVisibility && <p>Digite algo para criar o post</p>}
+      {postsList.length === 0 ? <p>Nenhum post</p> : <p>Quantidade de posts: {postsList.length}</p>}
       {postsList.map(post => {
         return (
           <Post
--- semana11/testes-react/src/App.test.js
+++ semana11/testes-react/src/App.test.js
@@ -1,54 +1,100 @@
 import React from "react";
 import '@testing-library/jest-dom/extend-expect'
-import { render, fireEvent, getByTestId, getByPlaceholderText, findByTestId, getByText, queryAllByText, waitForElement } from "@testing-library/react";
+import { render, fireEvent, getByTestId, getByPlaceholderText, findByTestId, getByText, queryAllByText, waitForElement, queryByTestId, queryByText, screen } from "@testing-library/react";
 import App from "./App";
 
-const createTestPost = (getByText, getByPlaceholderText, post) => {
-    const inputText = getByPlaceholderText(/novo post/i)
-    const addButton = getByText(/adicionar/i);
+const createTestPostAndRender = () => {
+    const utils = render(<App/>)
+    const inputText = utils.getByPlaceholderText(/novo post/i)
+    const addButton = utils.getByText(/adicionar/i);
+    fireEvent.change(inputText, {target: {value: "Texto do post" }});
+    fireEvent.click(addButton);
+
+    return utils
+}
+
+const createTestPost = (post) => {
+    const inputText = screen.getByPlaceholderText(/novo post/i)
+    const addButton = screen.getByText(/adicionar/i);
     fireEvent.change(inputText, {target: {value: post }});
     fireEvent.click(addButton);
 
 }
 
 test('Checa se um novo item é adicionado à lista de posts no clique em Adicionar', () => {
-    const { getByText, getByPlaceholderText } = render(<App/>);
-    
-    createTestPost(getByText, getByPlaceholderText, 'Texto do post.');
+    const { findByTestId } = createTestPostAndRender();
     
     expect(findByTestId('post'));
 })
 
 test('Checa se o botão do post tem o texto trocado ao clicar em curtir', () => {
-    const { getByText, getByPlaceholderText } = render(<App/>);
-
-    createTestPost(getByText, getByPlaceholderText, 'Texto do post.');
+    const { getByText } = createTestPostAndRender();
 
     const likeButton = getByText('Curtir')
+
     fireEvent.click(likeButton)
 
     expect(likeButton).toHaveTextContent('Descurtir')
-    
-    })
+
+    fireEvent.click(likeButton)
+
+    expect(likeButton).toHaveTextContent('Curtir')
+})
     
 test('Checa se o botão apagar deleta o post', () => {
-    const { getByText, getByPlaceholderText, queryAllByTestId } = render(<App/>);
-
-    createTestPost(getByText, getByPlaceholderText, 'Texto do post.');
+    const { getByText, queryAllByTestId } = createTestPostAndRender();
 
     expect(queryAllByTestId('post').length).toEqual(1);
 
     const deleteButton = getByText(/apagar/i);
+
     fireEvent.click(deleteButton);    
 
     expect(queryAllByTestId('post')).toEqual([]);
 })
 
 test('Checa se o input é limpo após o clique em postar', () => {
-    const { getByText, getByPlaceholderText } = render(<App/>);
+    const { getByPlaceholderText } = createTestPostAndRender();
 
-    createTestPost(getByText, getByPlaceholderText, 'Texto do post.');
-    const inputValue = getByPlaceholderText(/novo post/i).value
+    const input = getByPlaceholderText(/novo post/i)
 
-    expect(inputValue).toBe("")
+    expect(input).toHaveValue("")
+})
+
+test('Checa se a mensagem nenhum post é exibida', () => {
+    const utils = render(<App/>)
+
+    utils.getByText(/nenhum post/i)
+
+    createTestPost("Texto do post")
+
+    expect(utils.queryByText(/nenhum post/i)).toBeNull()
+})
+
+test('Checa a quantidade de posts está correta', () => {
+    const utils = render(<App/>)
+
+    expect(utils.queryByText(/quantidade de posts/i)).toBeNull();
+
+    createTestPost("Texto do post");
+    createTestPost("Texto do post");
+
+    const amount = utils.getByText(/quantidade de posts/i)
+
+    expect(amount).toHaveTextContent("Quantidade de posts: 2")
+})
+
+test('Checa se a msg de erro aparece quando o usuário tenta criar um post vazio', () => {
+    const utils = render(<App/>)
+    
+    expect(utils.queryByText(/digite algo para criar o post/i)).toBeNull()
+
+    createTestPost("")
+
+    expect(utils.queryAllByTestId('post')).toEqual([])
+    utils.getByText(/digite algo para criar o post/i)
+    
+    createTestPost("um post")
+    expect(utils.queryAllByTestId('post').length).toBeGreaterThan(0)
+    expect(utils.queryByText(/digite algo para criar o post/i)).toBeNull()
 })
