939d6859827ab81652d5617c2de3e14fe9d6b99f
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-10-19 12:25:25 -0300

    exercicio 3 e desafios

--- semana15/aula-node/ex2/index.js
+++ semana15/aula-node/ex2/index.js
@@ -3,18 +3,22 @@ const number1 = Number(process.argv[3])
 const number2 = Number(process.argv[4])
 
 const doTheMath = (operation, number1, number2) => {
-    switch(operation) {
-        case 'add':
-            return `Resposta: ${number1+number2}`
-        case 'sub':
-            return `Resposta: ${number1-number2}`
-        case 'mult':
-            return `Resposta: ${number1*number2}`
-        case 'div':
-            return `Resposta: ${number1/number2}`
-        default:
-            return "Por favor, digite uma das 4 operações: add, sub, mult ou div"
+    if(operation && number1 && number2){
+        switch(operation) {
+            case 'add':
+                return `Resposta: ${number1+number2}`
+            case 'sub':
+                return `Resposta: ${number1-number2}`
+            case 'mult':
+                return `Resposta: ${number1*number2}`
+            case 'div':
+                return `Resposta: ${number1/number2}`
+            default:
+                return "Por favor, digite uma das 4 operações: add, sub, mult ou div"
+        }
+    } else {
+        return "Você deve digitar o nome da operação e os dois números"
     }
 }
 
-console.log(doTheMath(operation, number1, number2))
+console.log("\x1b[33m%s\x1b[0m", doTheMath(operation, number1, number2))
