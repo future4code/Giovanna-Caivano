fc60b8d414bc1a1cd4ee0684063f8060b67bdcec
Giovanna Caivano <giovannadottocaivano@gmail.com>
2020-10-21 08:43:42 -0300

    ex8

--- semana15/intro-ts/src/ex8.ts
+++ semana15/intro-ts/src/ex8.ts
@@ -34,21 +34,23 @@ function makeOrder(clientName: string, dishName: string): void {
     })
 }
 
-function calcProfit(orders: ticket[]): number {
-    let profit = 0
+function calcProfit(orders: ticket[], menu: dish[]): number {
+    const profitArray: number[] = []
 
-    for(let order of orders){
-        //A TERMINAR
+    for(let ticket of orders){
+        let orderedDish: dish | undefined = menu.find(dish => { return dish.name === ticket.dishName })
+        if(orderedDish) profitArray.push(orderedDish.sellingPrice-orderedDish.cost)
     }
+
+    let profit: number = profitArray.reduce((total, num) => { return total + num })
+
     return profit
 }
 
 addDishToMenu('Carbonara', 9, 45, ['massa', 'ovos', 'bacon', 'sal'])
 addDishToMenu('Frango Xadrez', 10, 35, ['frango', 'pimentão', 'cebola', 'amendoim', 'shoyu'])
 addDishToMenu('Hamburguer Vegano', 10, 35, ['burguer de falafel', 'maionese de girassol', 'alface', 'cheddar vegano', 'shimeji'])
-console.log(menu)
-console.log(findDishPriceByName('Carbonara'))
-console.log(findDishPriceByName('Brigadeiro'))
 makeOrder('Giovanna', 'Hamburguer Vegano')
-console.log(orders)
+makeOrder('Renato', 'Carbonara')
+console.log(calcProfit(orders, menu))
 
